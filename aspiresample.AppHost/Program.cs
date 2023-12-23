7a9dadddb16e3bcc2c26289c6fb43facc4d1469e
David Fowler <davidfowl@gmail.com>
2023-12-23 02:13:13 -0800

    A little cleaner - Pass the data via an environment variable

--- aspiresample.AppHost/Program.cs
+++ aspiresample.AppHost/Program.cs
@@ -1,17 +1,20 @@
+using Aspire.Hosting.Utils;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddContainer("tempo", "grafana/tempo", "2.3.1")
-    .WithServiceBinding(containerPort: 3200, hostPort: 3200, name: "tempo-http", scheme: "http")
-    .WithServiceBinding(containerPort: 4317, hostPort: 4007, name: "tempo-ingres-grpc", scheme: "http")
+var tempo = builder.AddContainer("tempo", "grafana/tempo", "2.3.1")
+    .WithServiceBinding(containerPort: 3200, hostPort: 3200, name: "http", scheme: "http")
+    .WithServiceBinding(containerPort: 4317, hostPort: 4007, name: "otlp", scheme: "http")
     .WithVolumeMount("../config/tempo.yml", "/etc/tempo.yaml", VolumeMountType.Bind)
     .WithVolumeMount("tempo", "/tmp/tempo", VolumeMountType.Named)
     .WithArgs("-config.file=/etc/tempo.yaml");
 
 var otel = builder.AddContainer("otel", "otel/opentelemetry-collector-contrib", "0.91.0")
-    .WithServiceBinding(containerPort: 4317, hostPort: 4317, name: "otel-grpc", scheme: "http") // Have to put the schema to HTTP otherwise the C# will complain about the OTEL_EXPORTER_OTLP_ENDPOINT variable
-    .WithServiceBinding(containerPort: 55679, hostPort: 9200, name: "otel-zpages-http", scheme: "http")
+    .WithServiceBinding(containerPort: 4317, hostPort: 4317, name: "grpc", scheme: "http") // Have to put the schema to HTTP otherwise the C# will complain about the OTEL_EXPORTER_OTLP_ENDPOINT variable
+    .WithServiceBinding(containerPort: 55679, hostPort: 9200, name: "zpages-http", scheme: "http")
     .WithVolumeMount("../config/otel.yml", "/etc/otel-collector-config.yaml", VolumeMountType.Bind)
-    .WithArgs("--config=/etc/otel-collector-config.yaml");
+    .WithArgs("--config=/etc/otel-collector-config.yaml")
+    .WithEnvironment("TEMPO", tempo.GetEndpoint("otlp"));
 
 var apiService = builder.AddProject<Projects.aspiresample_ApiService>("apiservice")
     .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", otel.GetEndpoint("otel-grpc"));
